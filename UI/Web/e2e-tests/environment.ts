8464835d274e4427d0f0c3c0394ed0df218898f4
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-12-09 07:25:16 -0600

    Set up a basic login flow. Need to figure out how to test a complex flow.

---
+++ UI/Web/e2e-tests/environment.ts
@@ -0,0 +1,8 @@
+/**
+ * This is public information - create a environment.local.ts file and use admin account there
+ */
+export const environment = {
+  baseUrl: 'https://demo.kavitareader.com/',
+  username: 'demouser',
+  password: 'Demouser64',
+};
--- UI/Web/e2e-tests/tests/Login/login.spec.ts
+++ UI/Web/e2e-tests/tests/Login/login.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from 'e2e-tests/pages/login-page';
+import {environment} from "../../environment";
 
-
-const url = 'https://demo.kavitareader.com/';
 
 test('has title', async ({ page }) => {
-  await page.goto(url);
+  await page.goto(environment.baseUrl);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Kavita/);
@@ -13,14 +12,14 @@ test('has title', async ({ page }) => {
 
 test('login functionality', async ({ page }) => {
   // Navigate to the login page
-  await page.goto(url);
+  await page.goto(environment.baseUrl);
 
   // Verify the page title
   await expect(page).toHaveTitle(/Kavita/);
 
   const loginPage = new LoginPage(page);
-  await loginPage.navigate();
-  await loginPage.login('demouser', 'Demouser64');
+  //await loginPage.navigate();
+  await loginPage.login(environment.username, environment.password);
 
   // Verify successful login by checking for Home on side nav
   await expect(page.locator('#null')).toBeVisible();
